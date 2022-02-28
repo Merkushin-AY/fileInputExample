4397359689f8b1beedfddace19be3d0ccc0e5c39
Alexey <alexeyyb@mail.ru>
2022-03-01 00:51:09 +0300

    Add drag drop (why not)

--- src/components/common/BaseFileInput.vue
+++ src/components/common/BaseFileInput.vue
@@ -1,10 +1,17 @@
 <template>
     <div class="fileInput">
         <h3>Load your images</h3>
-        <label class="fileInput_label">
+        <label
+            class="fileInput_label"
+            :class="{dragged: isDraggedOn}"
+            ref="label"
+        >
             <!-- Тут лучше использовать svg спрайт -->
             <svg class="fileInput_uploadImage" xmlns="http://www.w3.org/2000/svg"viewBox="0 0 24 24"><g><rect fill="none" height="24" width="24"/></g><g><path d="M7.4,10h1.59v5c0,0.55,0.45,1,1,1h4c0.55,0,1-0.45,1-1v-5h1.59c0.89,0,1.34-1.08,0.71-1.71L12.7,3.7 c-0.39-0.39-1.02-0.39-1.41,0L6.7,8.29C6.07,8.92,6.51,10,7.4,10z M5,19c0,0.55,0.45,1,1,1h12c0.55,0,1-0.45,1-1s-0.45-1-1-1H6 C5.45,18,5,18.45,5,19z"/></g>
             </svg>
+            <p class="fileInput_labelDraggedText">
+                Put your file slowly. It's a glass
+            </p>
             <BaseInput class="fileInput_input" type="file" :accept="accept" @change="onInputChange" multiple/>
         </label>
         <BaseFormError v-for="(errorText, idx) in errors" :key="idx">
@@ -57,11 +64,38 @@
             reader: null,
             files: [],
             errors: {},
+            isDraggedOn: false,
         }),
         computed: {
             formattedMaxFileSize() {
                 return this.formatSize(this.maxFileSize);
             },
+        },
+        mounted() {
+            this.$refs.label.addEventListener("dragenter", (e) => {
+                e.stopPropagation();
+                e.preventDefault();
+                this.isDraggedOn = true;
+            }, false);
+            this.$refs.label.addEventListener("dragleave", (e) => {
+                e.stopPropagation();
+                e.preventDefault();
+                this.isDraggedOn = false;
+            }, false);
+            this.$refs.label.addEventListener("dragover", (e) => {
+                e.stopPropagation();
+                e.preventDefault();
+                this.isDraggedOn = true;
+            }, false);
+            this.$refs.label.addEventListener("drop", (e) => {
+                e.stopPropagation();
+                e.preventDefault();
+                this.isDraggedOn = false;
+
+                const files = e.dataTransfer.files;
+
+                this.handleFiles(files);
+            }, false);
         },
         methods: {
             onInputChange(event) {
@@ -84,7 +118,7 @@
                     }
 
                     /**
-                     * @TODO: Сюда можно добавить иные виды валидации (обязательность, количество файлов)
+                     * @TODO: Сюда можно добавить иные виды валидации (обязательность, количество файлов, формат файлов)
                      */
 
 
@@ -147,7 +181,9 @@
     display: none;
 }
 .fileInput_label {
+    position: relative;
     display: flex;
+    flex-flow: column;
     align-items: center;
     justify-content: center;
     width: 100%;
@@ -159,8 +195,21 @@
     transition: background-color .3s ease;
     cursor: pointer;
 }
-.fileInput_label:hover {
-    background-color: hsla(0,0%,100%,.4);
+.fileInput_label:hover,
+.fileInput_label.dragged {
+    background-color: hsla(0, 0%, 100%, 0.4);
+}
+.fileInput_labelDraggedText {
+    position: absolute;
+    bottom: 0;
+    left: 50%;
+    transform: translateX(-50%);
+    opacity: 0;
+    margin: 0;
+    transition: opacity 0.6s;
+}
+.fileInput_label.dragged .fileInput_labelDraggedText {
+    opacity: 1;
 }
 .fileInput_uploadImage {
     fill: #ffffff;
